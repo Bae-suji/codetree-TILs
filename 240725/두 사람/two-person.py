5ade334e9c523a0baf1a0a566a27311d8d7c4fd7
Bae-suji <129612819+Bae-suji@users.noreply.github.com>
2024-07-25 16:22:20 +0900

    Title: 두 사람, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240725/두 사람/two-person.py
@@ -0,0 +1,11 @@
+a = input().split()
+b = input().split()
+a1 = int(a[0])
+a2 = a[1]
+b1 = int(b[0])
+b2 = b[1]
+
+if (a1 >= 19 or b1 >= 19) and (a2 == 'M' or b2 == 'M'):
+    print(1)
+else:
+    print(0)
