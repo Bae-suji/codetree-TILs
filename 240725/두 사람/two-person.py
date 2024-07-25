dbbd4450ceeb2cf0eac5d16713a6fafced1cc87e
Bae-suji <129612819+Bae-suji@users.noreply.github.com>
2024-07-25 16:23:30 +0900

    Title: 두 사람, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240725/두 사람/two-person.py
+++ 240725/두 사람/two-person.py
@@ -4,8 +4,8 @@ a1 = int(a[0])
 a2 = a[1]
 b1 = int(b[0])
 b2 = b[1]
-
-if (a1 >= 19 or b1 >= 19) and (a2 == 'M' or b2 == 'M'):
+b1 >= 19
+if (a1 >= 19 and a2 == 'M') or (b1 >= 19 and b2 == 'M'):
     print(1)
 else:
     print(0)
