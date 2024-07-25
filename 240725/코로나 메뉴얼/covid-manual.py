5fd5444e057eb1315f325a12d970fe7c4cc16eed
Bae-suji <129612819+Bae-suji@users.noreply.github.com>
2024-07-25 16:55:12 +0900

    Title: 코로나 메뉴얼, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240725/코로나 메뉴얼/covid-manual.py
+++ 240725/코로나 메뉴얼/covid-manual.py
@@ -9,20 +9,15 @@ b2 = int(b[1])
 c1 = c[0]
 c2 = int(c[1])
 
-if a1 == "Y" and b1 == "Y":
-    if a2 >= 37 and  b2 >= 37:
+if c1 == 'Y' and t1 >= 37:
+    # 첫 번째 사람이 A라면, 남은 두 사람 중 한 사람이라도 A면 됩니다.
+    if (c2 == 'Y' and t2 >= 37) or (c3 == 'Y' and t3 >= 37):
         print("E")
     else:
         print("N")
-
-if a1 == "Y" and c1 == "Y":
-    if a2 >= 37 and c2 >= 37:
-        print("E")
-    else:
-        print("N")
-
-if b1 == "Y" and c1 == "Y":
-    if b2 >= 37 and c2 >= 37:
+else:
+    # 첫 번째 사람이 A가 아니라면, 남은 두 사람 모두 A여야만 합니다.
+    if (c2 == 'Y' and t2 >= 37) and (c3 == 'Y' and t3 >= 37):
         print("E")
     else:
         print("N")
