2cdcf213f5dcc3c5b2869678ca45ebd6207c12ef
Bae-suji <129612819+Bae-suji@users.noreply.github.com>
2024-07-25 15:50:55 +0900

    Title: 특정 조건 세 정수 비교, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 240725/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
+++ 240725/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -3,7 +3,7 @@ a = int(num[0])
 b = int(num[1])
 c = int(num[2])
 
-if a < b and a < c:
+if a <= b and a <= c:
     d = 1
 else:
     d = 0
@@ -12,5 +12,5 @@ if a == b == c:
     e = 1
 else:
     e = 0
-    
+
 print(d,e)
