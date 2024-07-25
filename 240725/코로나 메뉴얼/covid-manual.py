002c7cadb18a1c3c35d988148777785b7f47800b
Bae-suji <129612819+Bae-suji@users.noreply.github.com>
2024-07-25 16:55:46 +0900

    Title: 코로나 메뉴얼, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240725/코로나 메뉴얼/covid-manual.py
+++ 240725/코로나 메뉴얼/covid-manual.py
@@ -1,13 +1,9 @@
-a = input().split()
-b = input().split()
-c = input().split()
-
-a1 = a[0]
-a2 = int(a[1])
-b1 = b[0]
-b2 = int(b[1])
-c1 = c[0]
-c2 = int(c[1])
+inp = input().split()
+c1, t1 = inp[0], int(inp[1])
+inp = input().split()
+c2, t2 = inp[0], int(inp[1])
+inp = input().split()
+c3, t3 = inp[0], int(inp[1])
 
 if c1 == 'Y' and t1 >= 37:
     if (c2 == 'Y' and t2 >= 37) or (c3 == 'Y' and t3 >= 37):
