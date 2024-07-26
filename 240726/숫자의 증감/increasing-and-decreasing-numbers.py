5bfe789f80c94f5f0e5de71b6bedfbacbbcae2fd
Bae-suji <129612819+Bae-suji@users.noreply.github.com>
2024-07-26 12:16:19 +0900

    Title: 숫자의 증감, Time: 138ms, Memory: 25MB, Status: Passed - CodeTree

--- 240726/숫자의 증감/increasing-and-decreasing-numbers.py
+++ 240726/숫자의 증감/increasing-and-decreasing-numbers.py
@@ -6,5 +6,5 @@ if c == 'A':
     for i in range(1,n+1):
         print(f"{i}",end=" ")
 else:
-    for i in range(n,0):
-        print(f"{i}",end=" ")
+    for j in range(n,0,-1):
+        print(f"{j}",end=" ")
