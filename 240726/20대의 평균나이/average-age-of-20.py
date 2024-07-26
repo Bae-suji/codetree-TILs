54a5815a7874ceade84d9c74322eb96abcbdc837
Bae-suji <129612819+Bae-suji@users.noreply.github.com>
2024-07-26 17:06:38 +0900

    Title: 20대의 평균나이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240726/20대의 평균나이/average-age-of-20.py
+++ 240726/20대의 평균나이/average-age-of-20.py
@@ -1,6 +1,4 @@
-sum_val = 0
-ave_val = 0
-cnt = 0
+sum_val,ave_val,cnt = 0,0,0
 
 while True:
     n = int(input())
@@ -11,5 +9,5 @@ while True:
     sum_val += n
     cnt += 1
     ave_val = sum_val/cnt   
-    
+
 print(f"%0.2f"%ave_val)
