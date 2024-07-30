5cedc0693b062b03ee99434debd3ad60647994a3
Bae-suji <129612819+Bae-suji@users.noreply.github.com>
2024-07-30 12:34:16 +0900

    Title: 약수가 세 개인 수, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240730/약수가 세 개인 수/numbers-with-three-divisors.py
+++ 240730/약수가 세 개인 수/numbers-with-three-divisors.py
@@ -2,9 +2,10 @@ num = input().split()
 start = int(num[0])
 end = int(num[1])
 val2 = 0
+sum_val = 0
 
 for i in range(start,end+1):
-    sum_val = 0
+    
     for j in range(1,i):
         if i % j == 0:
             sum_val += 1
