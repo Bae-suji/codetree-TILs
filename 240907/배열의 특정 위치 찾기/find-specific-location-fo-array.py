d143f895ee1652a5ac894ce8cd7294e3fd7cabd3
Bae-suji <129612819+Bae-suji@users.noreply.github.com>
2024-09-07 12:30:40 +0900

    Title: 배열의 특정 위치 찾기, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 240907/배열의 특정 위치 찾기/find-specific-location-fo-array.py
+++ 240907/배열의 특정 위치 찾기/find-specific-location-fo-array.py
@@ -1,9 +1,20 @@
-num = list(map(int().split()))
+num = list(map(int, input().split()))
 
-even_num = num[1::2]
-sum_val = sum(even_num)
+# 짝수 번째로 입력된 값의 합과 3의 배수 번째로 입력된 값의 합, 평균을 구하는 변수
+even_sum = 0
+third_sum = 0
+third_count = 0
 
-third_num = num[2::3]
-val = sum(third_num)/len(third_num)
+# 인덱스는 0부터 시작하므로 짝수 번째 값은 인덱스가 홀수(1, 3, 5, ...)인 값들이 해당됩니다.
+for i in range(10):
+    if (i + 1) % 2 == 0:  # 짝수 번째 값 (인덱스는 1, 3, 5, ...)
+        even_sum += num[i]
+    if (i + 1) % 3 == 0:  # 3의 배수 번째 값 (인덱스는 2, 5, 8)
+        third_sum += num[i]
+        third_count += 1
 
-print(sum_val,round(avg_val, 1))
+# 3의 배수 번째 값의 평균을 구합니다.
+third_avg = third_sum / third_count if third_count > 0 else 0
+
+# 결과를 출력합니다.
+print(even_sum, round(third_avg, 1))
