59d45c755d55edfcaffcbc8f2ced30b570229a5e
Bae-suji <129612819+Bae-suji@users.noreply.github.com>
2024-09-08 12:25:02 +0900

    Title: 100 도달하기, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240908/100 도달하기/reach-100.py
+++ 240908/100 도달하기/reach-100.py
@@ -3,11 +3,13 @@ arr = [0]*100
 arr[0] = 1
 arr[1] = n
 
-                 
-for i in range(2, 10):
-    if arr[i] <= 100: 
-        arr[i] = (arr[i - 1] + arr[i - 2])
-    else:
+i = 2
+while True:
+    arr[i] = arr[i - 1] + arr[i - 2]
+    if arr[i] > 100:
         break
+    i += 1
 
-print(' '.join(map(str, arr[:i])))
+arr[i] = arr[i - 1] + arr[i - 2]
+
+print(' '.join(map(str, arr[:i+1])))
